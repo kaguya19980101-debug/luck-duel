7adfe2081a5851245b440847c779422dd88e944d
kaguya19980101-debug <kaguya19980101@gmail.com>
2026-05-25 16:05:31 +0800

    20260525 1605
    20260525 1605

--- js/main.js
+++ js/main.js
@@ -637,7 +637,8 @@ window.test_clearCards = async function () {
     #card-detail-modal {
         display: none;
         position: fixed;
-        inset: 0;
+        top: 0; left: 0; right: 0; bottom: 0;
+        width: 100%; height: 100%;
         background: rgba(0,0,0,0.85);
         z-index: 9999999;
         justify-content: center;
